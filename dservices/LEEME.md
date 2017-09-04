bae506e7f672a05dc323654788436b0779137dcf
Velosergio <velozasergio@gmail.com>
2017-09-04 17:57:10 -0500

    Change Notes 0.0.2
    Updates

---
+++ dservices/LEEME.md
@@ -0,0 +1,26 @@
+# Dservice
+
+Plataforma de servicios profesionales 
+
+## Demo
+---
+## Installation
+---
+# clone the repo 
+---
+# go into app's directory and install dependencies:
+---
+# serve with hot reload at localhost:8080
+---
+# build for production with minification
+---
+# build for production and view the bundle analyzer ---
+
+## Features
+---
+## How can I support developers?
+---
+## Can I hire you guys?
+---
+## License
+---
--- public/js/dservices.js
+++ public/js/dservices.js
@@ -1,1 +1,140 @@
-!function(t){function i(n){if(e[n])return e[n].exports;var a=e[n]={i:n,l:!1,exports:{}};return t[n].call(a.exports,a,a.exports,i),a.l=!0,a.exports}var e={};i.m=t,i.c=e,i.d=function(t,e,n){i.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:n})},i.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return i.d(e,"a",e),e},i.o=function(t,i){return Object.prototype.hasOwnProperty.call(t,i)},i.p="",i(i.s=50)}({50:function(t,i,e){t.exports=e(51)},51:function(t,e){$(document).ready(function(){$(function(){$(".datepicker").datepicker({autoSize:!0,dateFormat:"yy-mm-dd"})}),$("#paises").change(function(t){$.get("departamentos/"+t.target.value,function(t,e){for($("#departamentos").empty(),i=0;i<t.length;i++)$("#departamentos").append("<option value='"+t[i].id+"'>"+t[i].name+"</option>")})}),$("#departamentos").change(function(t){$.get("/ciudades/"+t.target.value,function(t,e){for($("#ciudad_id").empty(),$("#ciudades_id").empty(),i=0;i<t.length;i++)$("#ciudad_id").append("<option value='"+t[i].id+"'>"+t[i].name+"</option>"),$("#ciudades_id").append("<option value='"+t[i].id+"'>"+t[i].name+"</option>")})}),$("#Buscarcontratistas").autocomplete({source:"/buscar/contratistas",minLength:1,select:function(t,i){$("#Buscarcontratistas").val(i.item.value),$("#contratistas_id").val(i.item.id)}}),$("#Buscarcontratistas").click(function(){$("#Buscarcontratistas").val(""),$("#contratistas_id").val("0")}),$("#serviciosContratistas").keyup(function(t){$.get("/buscar/servicioscontratistas?term="+$("#serviciosContratistas").val(),function(t){$("#tablaServicios").empty().html(t)})}),$("#serviciosContratistas").click(function(){$("#serviciosContratistas").val(""),$("#servicioscontratistas_id").val("0")}),$(".SolicitudId").click(function(t){})})}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 48);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 15:
+/***/ (function(module, exports) {
+
+$(document).ready(function () {
+  $(function () {
+    $(".datepicker").datepicker({ autoSize: true, dateFormat: "yy-mm-dd" });
+  });
+  //--------------------------------------paises
+  $("#paises").change(function (event) {
+    $.get("departamentos/" + event.target.value + "", function (response, state) {
+      $("#departamentos").empty();
+      for (i = 0; i < response.length; i++) {
+        $("#departamentos").append("<option value='" + response[i].id + "'>" + response[i].name + "</option>");
+      }
+    });
+  });
+  //-----------------------------------------departamentos
+  $("#departamentos").change(function (event) {
+    $.get("/ciudades/" + event.target.value + "", function (response, state) {
+      $("#ciudad_id").empty();
+      $("#ciudades_id").empty();
+      for (i = 0; i < response.length; i++) {
+        $("#ciudad_id").append("<option value='" + response[i].id + "'>" + response[i].name + "</option>");
+        $("#ciudades_id").append("<option value='" + response[i].id + "'>" + response[i].name + "</option>");
+      }
+    });
+  });
+
+  //-----------------------------------Buscar asesores----------------------------------------
+  $("#Buscarcontratistas").autocomplete({
+    source: "/buscar/contratistas",
+    minLength: 1,
+    select: function select(event, ui) {
+      $('#Buscarcontratistas').val(ui.item.value);
+      $('#contratistas_id').val(ui.item.id);
+    }
+  });
+
+  $("#Buscarcontratistas").click(function () {
+    $("#Buscarcontratistas").val("");
+    $("#contratistas_id").val("0");
+  });
+
+  //-----------------------------------Buscar Servicios----------------------------------------
+  $("#serviciosContratistas").keyup(function (event) {
+    $.get('/buscar/servicioscontratistas?term=' + $("#serviciosContratistas").val(), function (data) {
+      $("#tablaServicios").empty().html(data);
+    });
+  });
+  $("#serviciosContratistas").click(function () {
+    $("#serviciosContratistas").val("");
+    $("#servicioscontratistas_id").val("0");
+  });
+  //-------------------------------Seleccionar solicitudes-------------------------------------
+  $(".SolicitudId").click(function (event) {
+    console.log('Hola mundo');
+  });
+});
+
+/***/ }),
+
+/***/ 48:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(15);
+
+
+/***/ })
+
+/******/ });
